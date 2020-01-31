686d2fb3577b294d87e51fe9e2fd8da9bbe60a4c
Gremlin4544 <alve@mckethen.com>
2020-01-31 11:40:59 -0500

    added axios to list component - no data displayed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+const CharacterList = (props) => {
   // TODO: Add useState to track data from useEffect
+  const [char, setChar] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!    
+    const getChars = () => {
+      axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setChar(response.data.results);
+      })
+      .catch(error => {
+        console.log('Server error', error);
+      })
+    }
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {char.map(char => (
+        <CharacterCard key={char.id} char={char} />
+      ))}
     </section>
   );
 }
+
+export default CharacterList;
